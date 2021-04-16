b9361d9a05d5c1bf3b0ca0e1c194e6ed230ac0a1
Tristan François <tpv.francois@gmail.com>
2021-04-16 17:11:31 +0200

    write dist matrix

--- src/dist_l2v.py
+++ src/dist_l2v.py
@@ -1,5 +1,10 @@
+# %%
 import lang2vec.lang2vec as l2v
+import pandas as np
 
+# %%
 dist = l2v.distance('syntactic', list(l2v.LANGUAGES))
 
-print(dist)
+# %%
+df = np.DataFrame(dist)
+df.write_csv("../data/dist_l2v.csv")
