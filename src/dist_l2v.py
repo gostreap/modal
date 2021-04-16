3c3a0e8ae4143ac2f037a5f19f02a33b4b62f756
Tristan François <tpv.francois@gmail.com>
2021-04-16 17:23:19 +0200

    dist_l2v_syntactic

--- src/dist_l2v.py
+++ src/dist_l2v.py
@@ -2,12 +2,13 @@
 import lang2vec.lang2vec as l2v
 import pandas as np
 
-# %%
-dist = l2v.distance('syntactic', list(l2v.LANGUAGES))
+# Write distance matrix
+# dist = l2v.distance('syntactic', list(l2v.LANGUAGES))
+# df = np.DataFrame(dist)
+# df.to_csv("../data/dist_l2v_syntactic.csv")
 
-# %%
-df = np.DataFrame(dist)
-df.write_csv("../data/dist_l2v.csv")
+# Load distance matrix
+df = np.read_csv("../data/dist_l2v_syntactic.csv")
 
 # creating graphe_1.txt
 graphe_1 = open("Graphe_1.txt","a")
@@ -15,5 +16,7 @@ graphe_1. truncate(0)
 graphe_1.write("Source,Target,Id,Length\n")
 for i in range(len(dist)):
     #string = str(streets[i][0])+","+str(streets[i][1])+","+str(streets[i][2])+","+str(1/streets[i][3])
-    graphe_1.write(string+"\n")
+    #graphe_1.write(string+"\n")
 graphe_1.close()
+
+# %%
