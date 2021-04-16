33c594294be3570feec6abf91aa8dbd2ecdd1d9a
christiankotait <82218860+christiankotait@users.noreply.github.com>
2021-04-16 16:08:54 +0200

    Update dist_l2v.py

--- src/dist_l2v.py
+++ src/dist_l2v.py
@@ -2,4 +2,11 @@ import lang2vec.lang2vec as l2v
 
 dist = l2v.distance('syntactic', list(l2v.LANGUAGES))
 
-print(dist)
+# creating graphe_1.txt
+graphe_1 = open("Graphe_1.txt","a")
+graphe_1. truncate(0)
+graphe_1.write("Source,Target,Id,Length\n")
+for i in range(len(dist)):
+    #string = str(streets[i][0])+","+str(streets[i][1])+","+str(streets[i][2])+","+str(1/streets[i][3])
+    graphe_1.write(string+"\n")
+graphe_1.close()
