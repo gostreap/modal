28f6ff7ec1e032bf05d5b5110bbcd05458dfea32
Tristan François <tpv.francois@gmail.com>
2021-04-16 18:13:14 +0200

    fix dist

--- src/dist_l2v.py
+++ src/dist_l2v.py
@@ -9,12 +9,15 @@ import pandas as np
 # Load distance matrix
 df = np.read_csv("../data/dist_l2v_syntactic.csv")
 dist = df.to_numpy()
+dist = dist[:,1:]
 
 # creating graphe_1.txt
+epsilon = 0.00001
 graphe_1 = open("Graphe_1.txt","a")
 graphe_1. truncate(0)
 graphe_1.write("Source,Target,Id,Length\n")
 for i in range(len(dist)):
-    string = str(i)+","+str(i)+","+str(i*len(dist)+j)+","+str(1/dist[i][j])
-    graphe_1.write(string+"\n")
+    for j in range(len(dist)):
+        string = str(i)+","+str(i)+","+str(i*len(dist)+j)+","+str(1/(dist[i][j]+epsilon))
+        graphe_1.write(string+"\n")
 graphe_1.close()
