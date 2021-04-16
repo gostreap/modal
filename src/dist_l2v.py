4a30f934679f367c0bd952d6d3b868107e7048fb
Tristan François <tpv.francois@gmail.com>
2021-04-16 17:26:48 +0200

    dist numpy array

--- src/dist_l2v.py
+++ src/dist_l2v.py
@@ -1,4 +1,3 @@
-# %%
 import lang2vec.lang2vec as l2v
 import pandas as np
 
@@ -9,6 +8,7 @@ import pandas as np
 
 # Load distance matrix
 df = np.read_csv("../data/dist_l2v_syntactic.csv")
+dist = df.to_numpy()
 
 # creating graphe_1.txt
 graphe_1 = open("Graphe_1.txt","a")
@@ -16,7 +16,6 @@ graphe_1. truncate(0)
 graphe_1.write("Source,Target,Id,Length\n")
 for i in range(len(dist)):
     #string = str(streets[i][0])+","+str(streets[i][1])+","+str(streets[i][2])+","+str(1/streets[i][3])
-    #graphe_1.write(string+"\n")
+    # graphe_1.write(string+"\n")
+    pass
 graphe_1.close()
-
-# %%
