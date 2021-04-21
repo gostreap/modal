353c459968caa67cfd12e840b648a62db4c4f432
christiankotait <82218860+christiankotait@users.noreply.github.com>
2021-04-21 12:39:51 +0200

    Update dist_l2v.py

--- src/dist_l2v.py
+++ src/dist_l2v.py
@@ -1,5 +1,6 @@
-import lang2vec.lang2vec as l2v
+#import lang2vec.lang2vec as l2v
 import pandas as np
+import scipy
 
 # Write distance matrix
 # dist = l2v.distance('syntactic', list(l2v.LANGUAGES))
@@ -14,7 +15,13 @@ dist = df.to_numpy()
 graphe_1 = open("Graphe_1.txt","a")
 graphe_1. truncate(0)
 graphe_1.write("Source,Target,Id,Length\n")
-for i in range(len(dist)):
-    string = str(i)+","+str(i)+","+str(i*len(dist)+j)+","+str(1/dist[i][j])
-    graphe_1.write(string+"\n")
-graphe_1.close()
+# for i in range(len(dist)):
+#     for j in range(len(dist)):
+#         string = str(i)+","+str(i)+","+str(i*len(dist)+j)+","+str(1-dist[i][j])
+#         graphe_1.write(string+"\n")
+# graphe_1.close()
+
+vecDist = scipy.spatial.distance.squareform(dist)
+
+clustering = scipy.cluster.hierarchy.linkage(vecDist)
+
