48bb81f11ebd5bb7b7d7e54f4f853f278e72a7a6
Tristan François <tpv.francois@gmail.com>
2021-05-30 21:14:24 +0200

    neo4j work

--- src/new_neo4j.py
+++ src/new_neo4j.py
@@ -1,20 +1,93 @@
 import json
 import pandas as pd
 
+
+def load_wals_to_glottocode():
+    wals_to_glottocode = dict()
+    lines = open("../data/wals_to_glottocode.csv", "r").readlines()
+    for i in range(1, len(lines)):
+        walscode, glottocode = lines[i].strip().split(",")
+        wals_to_glottocode[walscode] = glottocode
+    return wals_to_glottocode
+
+def load_feature_list():
+    return [
+        feature.strip().upper()
+        for feature in open("../data/feature_list.txt", "r").readlines()
+    ]
+
+def load_and_clear_wals(wals_to_glottocode):
+    full_wals = json.load(open("../data/wals.json", "r"))
+    wals = dict()
+    for glottocode in wals_to_glottocode.values():
+        walscodes = [w for w in full_wals.keys() if (w in wals_to_glottocode and wals_to_glottocode[w] == glottocode)]
+        if len(walscodes) > 0:
+            wals[glottocode] = full_wals[max(walscodes, key=lambda k: len(k))]
+    return wals
+
+
+def create_nodes_lang(wals, wals_to_glottocode):
+    file = open("../data/neo4j_graph_new/nodes_lang.csv", "w")
+    file.write("Name,glottocode,Genus,Family,Latitude,Longitude\n")
+    for key, lang in wals.items():
+        file.write(
+            '"{}","{}","{}","{}",{},{}\n'.format(
+                lang["language"],
+                key,
+                lang["genus"],
+                lang["family"],
+                lang["latitude"],
+                lang["longitude"],
+            )
+        )
+    file.close()
+
+
+def create_nodes_features(wals, wals_to_glottocode):
+    pass
+
+
+def create_nodes_phonemes(phoible):
+    pass
+
+
+def create_edges_lang_features(wals, feature_list):
+    file = open("../data/neo4j_graph_new/edges_lang_features.csv", "w")
+    file.write("Source,Target,Value\n")
+    for key in wals.keys():
+        for feature in feature_list:
+            if "_{}_num".format(feature) in wals[key]:
+                file.write('"{}","{}","{}"\n'.format(key, feature, wals[key]["_{}_num".format(feature)]))
+    file.close()
+
+
+def create_edges_lang_phonemes(phoible, wals):
+    file = open("../data/neo4j_graph_new/edges_lang_phonemes.csv", "w")
+    file.write("glottocode,Phoneme,dataset\n")
+    for index, row in phoible.iterrows():
+        if row["Glottocode"] in wals:
+            file.write('"{}","{}","{}"\n'.format(row["Glottocode"], row["Phoneme"], row["Source"]).replace('"',''))    
+    file.close()
+
+
+# wals to glottocode correspondance
+wals_to_glottocode = load_wals_to_glottocode()
+
+feature_list = load_feature_list()
+
 # open Wals.json
-file_wals = open("../data/wals.json", )
-wals = json.load(file_wals)
+wals = load_and_clear_wals(wals_to_glottocode)
 
 # open phoible.csv
-df_phoible = pd.read_csv(r"../data/phoeble.csv", low_memory = False)
+phoible = pd.read_csv(r"../data/phoeble.csv", low_memory=False)
 
-wals_name = []
-for _, lang in wals.items():
-    wals_name.append(str.lower(lang["language"]))
-wals_name = set(wals_name)
-phoible_name = set(map(str.lower, df_phoible["LanguageName"]))
 
-print(len(wals_name.intersection(phoible_name)))
-languages = list(wals_name.intersection(phoible_name))
-languages.sort()
-print(languages)
+
+# create files for neo4j graph
+create_nodes_lang(wals, wals_to_glottocode)
+# create_nodes_features(wals, wals_to_glottocode)
+create_edges_lang_features(wals, feature_list)
+# create_nodes_phonemes(phoible)
+create_edges_lang_phonemes(phoible, wals)
+
+# print(len(set(wals_to_glottocode.values())), len(wals_to_glottocode))
