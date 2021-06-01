6db8a60df0075bf582eaa9d2e90503b59b3a4d17
christiankotait <82218860+christiankotait@users.noreply.github.com>
2021-06-01 22:42:18 +0200

    plot phonem_inventory weights

--- src/plot_neo4j.py
+++ src/plot_neo4j.py
@@ -5,8 +5,16 @@ import pandas as pd
 
 # %%
 def plot_phoneme_inventory_size():
-    data = pd.read_csv("../data/neo4j/phoneme_inventory_size.csv")
-    plt.hist(data["C"], bins=range(10, max(data["C"])))
+    data = pd.read_csv("../data/neo4j/phoneme_inventory_size_by_dataset.csv")
+    set_dataset = set(data["ds"])
+    grouped = data.groupby(data.ds)
+    for s in set_dataset:
+        histDataset = grouped.get_group(s)
+        plt.hist(histDataset["AVG(c)"], bins=range(int(min(histDataset["AVG(c)"])), int((max(histDataset["AVG(c)"])))+1))
+    plt.show()
 
 plot_phoneme_inventory_size()
+
 # %%
+
+
