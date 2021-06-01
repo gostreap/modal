e82cba75fc25b996f81638b21f435fd83e859e64
christiankotait <82218860+christiankotait@users.noreply.github.com>
2021-06-01 23:26:55 +0200

    plot vowel consonants

--- src/plot_neo4j.py
+++ src/plot_neo4j.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 # %%
 def plot_phoneme_inventory_size():
@@ -10,11 +11,20 @@ def plot_phoneme_inventory_size():
     grouped = data.groupby(data.ds)
     for s in set_dataset:
         histDataset = grouped.get_group(s)
-        plt.hist(histDataset["AVG(c)"], bins=range(int(min(histDataset["AVG(c)"])), int((max(histDataset["AVG(c)"])))+1))
+        plt.hist(histDataset["AVG(c)"], bins=range(int(min(histDataset["AVG(c)"])), int((max(histDataset["AVG(c)"])))+1),label=s, rwidth=0.5)
+    plt.xlabel("Phoneme Inventory Size", fontsize=12)
+    plt.ylabel("Number of Languages", fontsize=12)
+    plt.legend(title="Dataset")
     plt.show()
 
-plot_phoneme_inventory_size()
+# plot_phoneme_inventory_size()
 
 # %%
+def plot_vowel_consonant():
+    data = pd.read_csv("../data/neo4j/consonant_vowel_by_dialect.csv")
+    sns.set(rc={"figure.figsize": (15, 25)})
+    sns.scatterplot(data=data, x=data["Consonant"], y=data["Vowel"])
+    plt.show()
 
 
+plot_vowel_consonant()
